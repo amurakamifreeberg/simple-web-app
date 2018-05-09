f95fec7d58fc85fb59ac96a2d12f3196ab289cdb
amurakamifreeberg <amurakamifreeberg@gmail.com>
2018-05-09 19:08:43 -0400

    my commit message

---
+++ webapp.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// app.get('/andrew', function (req, res){
+//   res.send('Hello Andrew!');
+// });
+app.get('/andrew', function (req, res)  {
+  res.send('<h1>Hello Andrew!</h1><p>How are you?</p>');
+});
